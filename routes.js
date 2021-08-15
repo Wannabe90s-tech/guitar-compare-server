3567f63bdcd317f1298b20c0cc47163189767183
Supachai Misa <69750795+supachaimisa@users.noreply.github.com>
2021-08-15 20:06:15 +0700

    dev auth

--- routes.js
+++ routes.js
@@ -1,6 +1,9 @@
 const blankPage = require('./routes/blankPage')
 const test = require('./routes/test')
+const login = require('./routes/login')
+const auth = require('./config/auth')
 module.exports = function (app) {
-    app.use('/',blankPage)
-    app.use('/',test)
+    app.use('/', auth.autenticateToken , blankPage)
+    app.use('/', auth.autenticateToken , test)
+    app.use('/', auth.autenticateToken , login)
 }
--- routes/blankPage.js
+++ routes/blankPage.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/blankPage.controller');
-router.get('/blankPage', controller.getBlankPage )
+router.get('/blankPage', async (req ,res)=> {
+    let result = await controller.getBlankPage()
+    res.send(result)
+} )
 router.get('/blankPage/io', (req , res) => {
     res.send('supachai01')
 })
